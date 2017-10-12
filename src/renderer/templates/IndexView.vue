4e8fc1431c7ba36f450d3cdadc3c75b78fa3d485
sage.xue <job.xueqi@gmail.com>
2017-10-12 10:59:48 +0800

    将bar组件化

--- src/renderer/templates/IndexView.vue
+++ src/renderer/templates/IndexView.vue
@@ -6,14 +6,6 @@
     position: relative;
     overflow: hidden;
 }
-.layout_title {
-    float: left;
-    min-height: 60px;
-    width: 78%;
-    margin-left: 15px;
-    padding-top: 20px;
-    padding-left: 10px;
-}
 
 .layout-content {
     float: left;
@@ -33,26 +25,6 @@
 .layout-menu-left {
     background: #fff;
     /*min-width: 100px;*/
-}
-
-.layout-header {
-    height: 60px;
-    background: #fff;
-    box-shadow: 0 1px 1px rgba(0, 0, 0, .1);
-}
-
-.layout-logo-left {
-    height: 68px;
-    background: #fff;
-    line-height: 30px;
-    text-align: center;
-    vertical-align: middle;
-    overflow: hidden;
-    padding-top: 10px;
-}
-
-.fill_bar {
-    height:200px;
 }
 
 .layout-ceiling-main a {
@@ -76,10 +48,6 @@
     transition: width .2s ease-in-out;
 }
 
-.ivu-menu-item {
-    padding: 14px 40px;
-}
-
 </style>
 
 <template>
@@ -87,57 +55,15 @@
 <div class="layout" :class="{'layout-hide-text': true}">
     <Row type="flex">
         <Col span="2" class="layout-menu-left">
-            <Menu :active-name="setActive" theme="primary" width="auto" @on-select="routeTo">
-                <div class="layout-logo-left"><img id="logo" src="~@/assets/logo_small.png"></div>
-                <MenuItem name="index">
-                    <Icon type="ios-navigate" :size="iconSize"></Icon>
-                    <span class="layout-text">关于</span>
-                </MenuItem>
-                <MenuItem name="form">
-                    <Icon type="document" :size="iconSize"></Icon>
-                    <span class="layout-text">表单</span>
-                </MenuItem>
-                <MenuItem name="table">
-                    <Icon type="navicon" :size="iconSize"></Icon>
-                    <span class="layout-text">表格</span>
-                </MenuItem>
-                <MenuItem name="markdown-viewer">
-                    <Icon type="social-markdown" :size="iconSize"></Icon>
-                    <span class="layout-text">Markdown</span>
-                </MenuItem>
-                <MenuItem name="markdown-editor-1">
-                    <Icon type="edit" :size="iconSize"></Icon>
-                    <span class="layout-text">Md编辑器(1)</span>
-                </MenuItem>
-                <MenuItem name="markdown-editor-2">
-                    <Icon type="edit" :size="iconSize"></Icon>
-                    <span class="layout-text">Md编辑器(2)</span>
-                </MenuItem>
-                <MenuItem name="rtf">
-                    <Icon type="document-text" :size="iconSize"></Icon>
-                    <span class="layout-text">富文本框</span>
-                </MenuItem>
-                <MenuItem name="upload">
-                    <Icon type="ios-cloud-upload-outline" :size="iconSize"></Icon>
-                    <span class="layout-text">文件上传</span>
-                </MenuItem>
-                <MenuItem name="echarts">
-                    <Icon type="pie-graph" :size="iconSize"></Icon>
-                    <span class="layout-text">图表(Echarts)</span>
-                </MenuItem>
-                <div class="fill_bar"></div>
-            </Menu>
+            <left-bar></left-bar>
         </Col>
         <Col span="22">
             <Row>
                 <Col span="24">
-                    <div class="layout-header">
-                    </div>
+                    <header-bar></header-bar>
                 </Col>
             </Row>   
-            <div class="layout_title">
-                <h3>My Genaro Domain</h3>
-            </div> 
+            <title-bar></title-bar>
             <div class="layout-content">
                 <div class="layout-content-main">
                     <transition mode="out-in">
@@ -155,25 +81,16 @@
 </template>
 
 <script>
+import HeaderBar from '@/assembly/HeaderBar'
+import LeftBar from '@/assembly/LeftBar'
+import TitleBar from '@/assembly/TitleBar'
 
 export default {
-
-    data() {
-            return {
-                iconSize: 20
-            }
-        },
-        computed: {
-            setActive() {
-              return this.$route.path.replace('/','');
-            }
-        },
-        methods: {
-            routeTo(e) {
-                //console.log(e);
-                this.$router.push(e);
-            }
-        }
+    components:{
+        HeaderBar,
+        LeftBar,
+        TitleBar
+    }
 }
 
 </script>
