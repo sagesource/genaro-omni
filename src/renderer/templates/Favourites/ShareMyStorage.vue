a1013fc71907179ea81d22f09cfed36453ad4642
sage.xue <job.xueqi@gmail.com>
2017-10-31 13:46:44 +0800

    修改图片

--- src/renderer/templates/Favourites/ShareMyStorage.vue
+++ src/renderer/templates/Favourites/ShareMyStorage.vue
@@ -1,14 +1,15 @@
 <style scoped>
     #share-chart-img {
-        width: 580px;
-        height: 495px;
         margin-left: 22px;
+    }
+    #share-note {
+        margin-left: 30px;
     }
 </style>
 <template>
     <div id="wrap">
         <span><img id="share-chart-img" src="~@/assets/share_chart_img.png" title="Note: This feature is not available for Genaro Eden Beta"></span>
-        <h4>Note: This feature is not available for Genaro Eden Beta</h4>
+        <h4 id="share-note">Note: This feature is not available for Genaro Eden Beta</h4>
     </div>
 </template>
 
